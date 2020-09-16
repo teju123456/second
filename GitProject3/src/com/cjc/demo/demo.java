e1914e62f450d9703604717dab88bc5a3d3a8745
A <A@DESKTOP-R4E23KQ>
2020-09-16 05:56:24 -0500

    pooja

--- GitProject3/src/com/cjc/demo/demo.java
+++ GitProject3/src/com/cjc/demo/demo.java
@@ -1,6 +1,18 @@
 package com.cjc.demo;
 
 public class demo {
+	public void addition()
+	{
+		System.out.println("addition is");
+		
+	}
+	public void subtraction()
+	{
+		System.out.println("subtraction is");
+		
+	}
+	
+	
 	public static void main(String[] args) {
 		System.out.println("hello");
 	}
